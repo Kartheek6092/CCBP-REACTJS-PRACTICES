75bb61ae398c7a1d843b830ba715718bcf289d38
Kartheek6092 <kartheek6092@gmail.com>
2023-04-08 10:32:04 +0530

    creating superOverLeagueApp

---
+++ superOverLeagueApp/index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="teams">
+    <h1>Super Over League</h1>
+    <span>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="Royal challengers"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="Chennai super kings"
+      />
+    </span>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
