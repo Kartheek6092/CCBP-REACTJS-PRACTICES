745f067c5ddb39c9381530c4bf718d6c25a8b608
Kartheek6092 <kartheek6092@gmail.com>
2023-06-26 10:18:29 +0530

    creating ebank login App

---
+++ Interview-Mock-Test-4/ebank/src/components/Home/styledComponents.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #152850;
+  color: #ffffff;
+`
+
+export const Header = styled.nav`
+  width: 100%;
+  height: 10vh;
+  padding: 5% 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Logo = styled.img`
+  width: 10%;
+  height: fit-content;
+`
+
+export const LogoutBtn = styled.button`
+  width: 10%;
+  padding: 15px;
+  background-color: transparent;
+  border: 2px solid #e0eefe;
+  color: #e0eefe;
+  outline: none;
+  cursor: pointer;
+`
+
+export const MainHeading = styled.h1`
+  color: #ffffff;
+`
+export const DigitalCard = styled.img`
+  width: 50%;
+  height: fit-content;
+`
