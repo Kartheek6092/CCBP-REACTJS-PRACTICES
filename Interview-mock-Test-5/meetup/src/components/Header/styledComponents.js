169e41f6ffb1873a647de243f9699c3e0b9d8824
Kartheek6092 <kartheek6092@gmail.com>
2023-06-27 22:55:11 +0530

    creating Meetup App

---
+++ Interview-mock-Test-5/meetup/src/components/Header/styledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.nav`
+  width: 100vw;
+  height: 15vh;
+  display: flex;
+  padding: 2% 10px;
+`
+export const Logo = styled.img`
+  width: fit-content;
+  height: 100%;
+`
